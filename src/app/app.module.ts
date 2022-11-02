5260a207f268fefd0b3e4e1459959842915cd369
Lathathadiparthi <105854631+Lathathadiparthi@users.noreply.github.com>
2022-11-02 23:41:04 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import { Component } from '@angular/core';
+ import {  MatButtonModule} from '@angular/material/button';
+import {MatCardModule}  from '@angular/material/card'
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      BrowserAnimationsModule,
+    ReactiveFormsModule,
+     MatButtonModule,
+      MatCardModule,
+      MatTableModule,
+      MatFormFieldModule,
+      MatIconModule
+      
   ],
-  providers: [],
+    providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
