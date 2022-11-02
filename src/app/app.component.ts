ea4649d7d24f2c6d03e47d993d9623af50420a26
Lathathadiparthi <105854631+Lathathadiparthi@users.noreply.github.com>
2022-11-02 23:39:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginform';
+  
+  
+  loginForm:any;
+  public showPassword: boolean = false;
+
+
+   constructor() {}
+    OnInit() {
+      this.loginForm= new FormGroup (
+        {
+        email: new FormControl ('',[Validators.required,Validators.email]),
+        password: new FormControl('',[Validators.required,Validators.minLength(6)])
+        }
+      )
+      
+    }
+  onLogin() {
+     if (!this.loginForm.valid) {
+      return;
+     }
+     console.log(this.loginForm.value);
+     }
+
+     public togglePasswordVisibility(): void {
+      this.showPassword = !this.showPassword;
+     }
+
 }
+
+
+   
+  
